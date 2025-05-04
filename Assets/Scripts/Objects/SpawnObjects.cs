208c18dd5f7af796d7b365092e080633c6ced1e8
Keita58 <marc-s-26@hotmail.com>
2025-05-04 20:04:12 +0200

    Creats els ítems restants + scripts + SO d'aquests

---
+++ Assets/Scripts/Objects/SpawnObjects.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class SpawnObjects : MonoBehaviour
+{
+    [SerializeField] private Transform[] ImportantSpawns;
+    [SerializeField] private Transform[] NormalSpawns;
+
+    [SerializeField] private Item[] ImportantObjects;
+    [SerializeField] private Item[] NormalObjects;
+
+    void Start()
+    {
+        
+    }
+}
