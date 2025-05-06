8a484f2469b583618388f038dc66e07fdd5f4e6a
Keita58 <marc-s-26@hotmail.com>
2025-05-06 17:42:24 +0200

    Objectes procedurals afegits

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -6,4 +6,6 @@ public abstract class Enemy : MonoBehaviour
     public abstract int downTime { get; }
 
     public abstract void ListenSound(Vector3 pos, int lvlSound);
+
+    public abstract void TakeHealth();
 }
