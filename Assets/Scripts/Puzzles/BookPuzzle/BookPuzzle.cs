ee6302f5363eec0700226a0893e56f480fd54ea0
eric <ericpro978@gmail.com>
2025-05-27 20:18:38 +0200

    solucionant bug i audio

--- Assets/Scripts/Puzzles/BookPuzzle/BookPuzzle.cs
+++ Assets/Scripts/Puzzles/BookPuzzle/BookPuzzle.cs
@@ -8,11 +8,20 @@ public class BookPuzzle : MonoBehaviour
     [SerializeField]
     List<CellBook> bookList;
 
+    [Header("Audio")]
+    AudioSource bookAudioSource;
+    [SerializeField]
+    AudioClip finishPuzzle;
+    private void Awake()
+    {
+        bookAudioSource = GetComponent<AudioSource>();
+    }
     public void checkBookPosition()
     {
         if (bookList.ElementAt(0).GetBook() != null && bookList.ElementAt(1).GetBook() != null && bookList.ElementAt(2).GetBook() != null && bookList.ElementAt(3).GetBook() != null && bookList.ElementAt(4).GetBook() != null){
             if (bookList.ElementAt(0).GetBook().GetComponent<PickItem>().item.ItemType == ItemTypes.BOOK2 && bookList.ElementAt(1).GetBook().GetComponent<PickItem>().item.ItemType == ItemTypes.BOOK4 && bookList.ElementAt(2).GetBook().GetComponent<PickItem>().item.ItemType == ItemTypes.BOOK3 && bookList.ElementAt(3).GetBook().GetComponent<PickItem>().item.ItemType == ItemTypes.BOOK5 && bookList.ElementAt(4).GetBook().GetComponent<PickItem>().item.ItemType == ItemTypes.BOOK1)
             {
+                bookAudioSource.PlayOneShot(finishPuzzle);
                 PuzzleManager.instance.BookPuzzleCompleted();
                 Debug.Log("Lo has completado!!!!!!");
             }
